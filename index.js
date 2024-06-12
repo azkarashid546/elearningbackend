33639dbbcc63a188a8b3a9318d4b54d131eeab3d
Azka Rashid <124484697+azkarashid546@users.noreply.github.com>
2024-06-12 20:58:22 +0500

    a bunch of changes

--- index.js
+++ index.js
@@ -1,52 +1,76 @@
 const express = require('express');
 const session = require('express-session');
 const cors = require('cors');
-const cookieParser = require("cookie-parser");
-require("dotenv").config();
-const connectToMongo = require("./utils/db");
-const ErrorMiddleware = require("./middleware/error");
-const router = require('./routes/user');
-const CourseRouter = require("./routes/course");
-const orderRouter = require("./routes/order");
-const notificationRouter = require("./routes/notification");
-const analyticsRouter = require("./routes/analytics");
-const cloudinary = require("cloudinary").v2; // Added .v2
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const connectToMongo = require('./utils/db');
+const ErrorMiddleware = require('./middleware/error');
+const userRouter = require('./routes/user');
+const courseRouter = require('./routes/course');
+const orderRouter = require('./routes/order');
+const notificationRouter = require('./routes/notification');
+const analyticsRouter = require('./routes/analytics');
+const cloudinary = require('cloudinary').v2;
 const layoutRouter = require('./routes/layout');
-const passport = require("passport");
-const http = require("http");
+const passport = require('passport');
+const http = require('http');
 const initSocketServer = require('./socketServer');
-const UploadCertifcateRouter = require('./routes/uploadCertifcate');
+const uploadCertificateRouter = require('./routes/uploadCertifcate');
 const contactRouter = require('./routes/contactUs');
+const chatRouter = require('./routes/chatgpt');
 
 const app = express();
 
 connectToMongo();
 
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
-        secure: process.env.NODE_ENV === 'production', // set to true in production
-        httpOnly: true,
-        maxAge: 31536000000, // 1 year
+      secure: process.env.NODE_ENV === 'production',
+      httpOnly: true,
+      maxAge: 31536000000,
     },
-}));
+  })
+);
 
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 
-app.use(cors());
+app.use(
+  cors()
+);
 
-// Cloudinary config
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_SECRET_KEY
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_SECRET_KEY,
 });
 
-// Available Routes
-app.use('/api/v1', router, CourseRouter, orderRouter, notificationRouter, analyticsRouter, layoutRouter, UploadCertifcateRouter, contactRouter);
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
+
+app.use(
+  '/api/v1',
+  userRouter,
+  courseRouter,
+  orderRouter,
+  notificationRouter,
+  analyticsRouter,
+  layoutRouter,
+  uploadCertificateRouter,
+  contactRouter,
+  chatRouter
+);
+
+app.get('/test-cors', (req, res) => {
+  res.send('CORS is working!');
+
+});
+
 
 app.use(ErrorMiddleware);
 
@@ -54,6 +78,7 @@ const server = http.createServer(app);
 
 initSocketServer(server);
 
-server.listen(process.env.PORT, () => {
-    console.log(`Server is connected on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
